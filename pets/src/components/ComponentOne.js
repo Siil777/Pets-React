87fd6be3cbc1be02f7efb88b142b9c813d4dfff9
Siil777 <pavel.ivanov.thk@gmail.com>
2024-09-05 16:17:13 +0300

    working on first section

--- pets/src/components/ComponentOne.js
+++ pets/src/components/ComponentOne.js
@@ -6,20 +6,22 @@ import  '../css/ComponentOne.css';
 
 const One =()=> {
     return(
-        <div>
-            <div className='point mx-md-auto d-block d-lg-flex  d-grid mt-5 justify-content-lg-between justify-content-sm-center'>
-            <div className='not-only'>
-                <h2>Not only people<span className='d-block'>need a home</span></h2>
-                <p className='mt-5'>We offer to give a chance to a little and nice puppy with an extremely wide and open heart. 
-                    He or she will love you more than anybody else in the world, you will see!
-                </p>
-                <div id='btn' className='btn-one col-md-4 d-flex justify-content-center align-items-center p-2'><a>Make a friend</a></div>
+        <Container>
+            <div>
+                <div className='point mx-md-auto d-block d-lg-flex  d-grid mt-5 justify-content-lg-between justify-content-sm-center'>
+                    <div className='not-only'>
+                        <h2>Not only people<span className='d-block'>need a home</span></h2>
+                        <p className='mt-5'>We offer to give a chance to a little and nice puppy with an extremely wide and open heart.
+                            He or she will love you more than anybody else in the world, you will see!
+                        </p>
+                        <div id='btn' className='btn-one col-sm-4 col-md-4 d-flex justify-content-center align-items-center p-2'><a>Make a friend</a></div>
+                    </div>
+                    <div className='mt-5'>
+                        <img src={puppy} alt='puppy' height={'450px'}></img>
+                    </div>
+                </div>
             </div>
-            <div className='mt-5'>
-                <img src={puppy} alt='puppy' height={'450px'}></img>
-            </div>
-        </div>
-        </div>
+        </Container>
     );
 }
 export default One;
--- pets/src/components/Header.js
+++ pets/src/components/Header.js
@@ -1,22 +1,40 @@
-import {NavDropDown, Nav, Container, Navbar} from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Nav, Container, Navbar } from 'react-bootstrap';
+import Hamburger from 'hamburger-react';
 import '../App.css';
+import '../css/Header.css';
 
-const Header =()=> {
+const Header = () => {
+  console.log(useState);
+  const [isOpen, setOpen] = useState(false);
+
   return (
-        <header>
-          <nav className='d-flex justify-content-between'>
-            <div>
-              <h1>Cozy House</h1>
-              <h6>Shelter for pets in Boston</h6>
-            </div>
-            <div className='d-flex gap-4'>
-              <a href='#'>About the shelter</a>
-              <a href='#'>Our pets</a>
-              <a href='#'>Help the shelter</a>
-              <a href='#'>Contacts</a>
-            </div>
-          </nav>
-        </header>
+    <Navbar expand='lg' className='d-flex justify-content-between'>
+        <Navbar.Brand href='#'>
+          Cozy House
+          <h6>Shelter for pets in Boston</h6>
+        </Navbar.Brand>
+        <div className='d-lg-none custom-ham'>
+        <Hamburger color='var(--color-primary)' toggled={isOpen} toggle={setOpen} />
+        </div>
+        <Navbar.Toggle
+          aria-controls='basic-navbar-nav'
+          aria-expanded={isOpen}
+          onClick={() => setOpen(prev => !prev)}
+        />
+        <Navbar.Collapse
+          id='basic-navbar-nav'
+          className={isOpen ? 'show' : ''} style={{flexGrow: '0'}}
+        >
+          <Nav>
+            <Nav.Link href='#'>About the shelter</Nav.Link>
+            <Nav.Link href='#'>Our pets</Nav.Link>
+            <Nav.Link href='#'>Help the shelter</Nav.Link>
+            <Nav.Link href='#'>Contacts</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+    </Navbar>
   );
 }
+
 export default Header;
