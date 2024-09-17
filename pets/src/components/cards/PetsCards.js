2d8f2c4b4ad0df5059d35c5d3e83c0790515d036
Siil777 <pavel.ivanov.thk@gmail.com>
2024-09-17 19:57:06 +0300

    working with Mui making cards

--- pets/src/components/cards/PetsCards.js
+++ pets/src/components/cards/PetsCards.js
@@ -1,52 +1,59 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
+import Card from '@mui/material/Card'
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import { CardActions, Typography } from '@mui/material';
 
-const Cards=()=>{
-    const [petList,setPetList] = useState([]);
+const Cards = () => {
+    const [petList, setPetList] = useState([]);
     const [currIndex] = useState(3);
 
-     useEffect(()=>{
+    useEffect(() => {
         const fetchPets = async () => {
-       try{
-            const response = await fetch('/Pets-React/pets.json');
-            console.log('response out here', response);
-            if(!response.ok){
-                throw new Error('response must be Json not html');
-            }
-            const data = await response.json();
-            console.log(data);
-            setPetList(data);
-       }catch(e){
-        console.error(e);
-       };
-    };
-       fetchPets();
+            try {
+                const response = await fetch('/Pets-React/pets.json');
+                console.log('response out here', response);
+                if (!response.ok) {
+                    throw new Error('response must be Json not html');
+                }
+                const data = await response.json();
+                console.log(data);
+                setPetList(data);
+            } catch (e) {
+                console.error(e);
+            };
+        };
+        fetchPets();
     }, []);
     const visible = petList.slice(0, currIndex);
-    if(petList.length===0){
+    if (petList.length === 0) {
         return <p>...Loading</p>
-    }  
-    return(
-        <div className='d-flex justify-content-center gap-5'>
-            {visible.map(card=>{
+    }
+    return (
+        <div style={{ display: "flex", gap: "100px", justifyContent: "center" }}>
+            {visible.map(card => {
                 console.log(card.img);
-                return(
-                    <div key={card.id}>
-                         <div className=''>
-                            <img src={card.img} alt={card.name}/>
-                            <div className='text-center'>
+                return (
+                    <Card key={card.id}>
+                        <CardMedia
+                            sx={{ height: 245, width: 245 }}
+                            image={card.img}
+                        />
+                        <CardContent>
+                            <Typography className='text-center' gutterBottom variant="h5" component='div'>
                                 {card.name}
-                            <div>
-                                <Button color="secondary">Learn more</Button>
-                            </div>
-                            </div>
-                        </div>
-                    </div>
+                            </Typography>
+                        </CardContent>
+                        <CardActions className='d-flex justify-content-center'>
+                            <Button size='small'>Learn more</Button>
+                        </CardActions>
+                    </Card>
                 )
             })}
         </div>
     )
-    
+
 }
 
 export default Cards;
